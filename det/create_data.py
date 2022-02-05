bfef62708e03bb40964c220f25228ff392186685
Dekun Ma <william@dekun.me>
2022-02-05 16:02:47 -0500

    Add det scripts

--- det/create_data.py
+++ det/create_data.py
@@ -1,7 +1,11 @@
 import os
 
-for ii in range(6):
-    os.system(f'sbatch create_data.sh {ii} {ii} 0 20')
-    os.system(f'sbatch create_data.sh {ii} {ii} 20 40')
-    os.system(f'sbatch create_data.sh {ii} {ii} 40 80')
+# for ii in range(6):
+#     # os.system(f'sbatch create_data.sh {ii} {ii} 0 20')
+#     # os.system(f'sbatch create_data.sh {ii} {ii} 20 40')
+#     # os.system(f'sbatch create_data.sh {ii} {ii} 40 80')
+#     os.system(f'sbatch create_data.sh {ii} {ii} 90 100')
+
+os.system(f'sbatch create_data.sh 1 1 90 100')
+
 
