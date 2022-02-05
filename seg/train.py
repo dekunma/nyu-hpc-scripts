1a76490c2e5624aa97b7823e353d9d1ffda6feb7
Dekun Ma <william@dekun.me>
2022-02-05 16:03:08 -0500

    Update seg

--- seg/train.py
+++ seg/train.py
@@ -2,16 +2,16 @@ import os
 
 tasks = (
     # ('l', 'train_lowerbound'),
-    ('l_nc', 'train_lowerbound_nc'),
+    # ('l_nc', 'train_lowerbound_nc'),
     # ('u', 'train_upperbound'),
-    ('u_nc', 'train_upperbound_nc'),
+    # ('u_nc', 'train_upperbound_nc'),
     # ('v', 'train com=V2V'),
-    ('v_nc', 'train_nc com=V2V'),
+    # ('v_nc', 'train_nc com=V2V'),
     # ('w', 'train com=when2com'),
     ('w_nc', 'train_nc com=when2com'),
-    # ('ww', 'train_warp com=when2com'),
+    ('ww', 'train_warp com=when2com'),
     ('ww_nc', 'train_nc_warp com=when2com')
 )
 
 for t in tasks:
-    os.system(f'source ./train.sh {t[0]} {t[1]}')
+    os.system(f'source ./train.sh {t[0]} \'{t[1]}\'')
---
+++ seg/train_resume.py
@@ -0,0 +1,17 @@
+import os
+
+tasks = (
+    ('l', 'train_bound_resume bound=lowerbound resume_epoch_num=89'),
+    # ('l_nc', 'train_lowerbound_nc'),
+    ('u', 'train_bound_resume bound=upperbound resume_epoch_num=89'),
+    # ('u_nc', 'train_upperbound_nc'),
+    ('v', 'train_resume com=V2V resume_epoch_num=33'),
+    # ('v_nc', 'train_nc com=V2V'),
+    # ('w', 'train com=when2com'),
+    # ('w_nc', 'train_nc com=when2com'),
+    # ('ww', 'train_warp com=when2com'),
+    # ('ww_nc', 'train_nc_warp com=when2com')
+)
+
+for t in tasks:
+    os.system(f'source ./train_resume.sh {t[0]} \'{t[1]}\'')
